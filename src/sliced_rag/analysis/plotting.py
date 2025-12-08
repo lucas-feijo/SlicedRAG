efc0f9c80aa2c11759d724187e0b1237d20edcbe
lucas-feijo <lucas@feijoandrade.com>
2025-12-08 20:34:07 +0100

    plot utils

--- src/sliced_rag/analysis/plotting.py
+++ src/sliced_rag/analysis/plotting.py
@@ -1,6 +1,17 @@
 import matplotlib.pyplot as plt
 
 def plot_sparsity_metric(df, metric, metric_title, model_title, scale=None):
+    """
+    Plot metric vs model sparsity.
+
+    Args:
+        df (pandas.DataFrame): Dataframe containing sparsity metrics. Dataframe should be in the format
+        used in utils.load_sparsity_eval_results().
+        metric (string): Name of dataframe column containing the metric values.
+        metric_title (string): Title to be used for the metric axis in the plot.
+        model_title (string): Model name to be used in plot title.
+        scale (tuple of int or float): minimum and maximum values for the y axis. Default: [0, 1]
+    """
     if scale is None:
         scale = [0, 1]
 
@@ -14,4 +25,4 @@ def plot_sparsity_metric(df, metric, metric_title, model_title, scale=None):
     ax = plt.gca()
     ax.set_ylim(scale)
 
-    plt.show()
+    return plt
